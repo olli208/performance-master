6f6ab1e01b45988b21559efabe525e411fd2a61e
Oliver <hamburger602@gmail.com>
2017-03-15 11:45:01 +0100

    clean/concat CSS

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var concatCSS = require('gulp-concat-css');
+
+gulp.task('CSS', function () {
+    return gulp.src(['src/dist/css/*.css' , 'src/assets/css/src/docs.css'])
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(concatCSS("bundle.css"))
+        .pipe(gulp.dest('src/dist/css'));
+});
