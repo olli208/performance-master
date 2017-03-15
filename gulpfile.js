9040312747e4546214679773f81e864f8a69bbba
Oliver <hamburger602@gmail.com>
2017-03-15 14:05:38 +0100

    optimized images for web

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var concatCSS = require('gulp-concat-css');
 var uglifyJS = require('gulp-uglify');
 var pump = require('pump');
 var concatJS = require('gulp-concat');
-var imagemin = require('gulp-imagemin');
 var image = require('gulp-image');
+var webp = require('gulp-webp');
 
 gulp.task('CSS', function () {
     return gulp.src(['src/dist/css/*.css' , 'src/assets/css/src/docs.css'])
-        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(concatCSS("bundle.css"))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('src/dist/css'));
 });
 
@@ -36,6 +36,12 @@ gulp.task('image', function() {
         .pipe(gulp.dest('src/assets/img/'));
 });
 
+gulp.task('webp', function () {
+    return gulp.src('src/assets/img/*')
+        .pipe(webp())
+        .pipe(gulp.dest('src/assets/img/webp/'));
+});
 
 
 gulp.task('JS' , ['uglifyJS' , 'concatJS']);
+gulp.task('images' , ['image' , 'webp']);
